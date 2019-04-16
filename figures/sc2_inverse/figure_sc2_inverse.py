3603294219da9f27b15327c56d62fec6630d615b
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2019-04-16 16:30:43 -0400

    sc2 inverse images plus grandes

--- figures/sc2_inverse/figure_sc2_inverse.py
+++ figures/sc2_inverse/figure_sc2_inverse.py
@@ -92,7 +92,7 @@ ax.arrow(
 )
 
 ax.text(
-    distances1[max1] - 0.4,
+    distances1[max1] - 0.35,
     reaction1[max1] / 2,
     "{:4.2f} eV".format(reaction1[max1]),
     fontsize=25,
@@ -110,8 +110,7 @@ image3 = Image.open("posinp17.jpg")
 image4 = Image.open("posinp24.jpg")
 image5 = Image.open("posinp33.jpg")
 
-zoom = 0.24
-zoom2 = 0.27
+zoom, zoom2 = 0.26, 0.29
 imagebox1 = OffsetImage(image1, zoom=zoom)
 imagebox2 = OffsetImage(image2, zoom=zoom)
 imagebox3 = OffsetImage(image3, zoom=zoom)
@@ -133,19 +132,19 @@ ab1 = AnnotationBbox(
 ab2 = AnnotationBbox(
     imagebox2,
     [distances1[max1] - 0.02, reaction1[max1] + 0.01],
-    xybox=[0.28 * distances2[-1], 4.3],
+    xybox=[0.28 * distances2[-1], 4.2],
     arrowprops=dict(arrowstyle="->", linewidth=4),
 )
 ab3 = AnnotationBbox(
     imagebox3,
     [distances1[-1], reaction1[-1] + 0.02],
-    xybox=[distances1[-1], 4.3],
+    xybox=[distances1[-1], 4.2],
     arrowprops=dict(arrowstyle="->", linewidth=4),
 )
 ab4 = AnnotationBbox(
     imagebox4,
     [distances2[max2] + 0.02, reaction2[max2] + 0.01],
-    xybox=[0.98 * distances2[-1], 4.3],
+    xybox=[0.98 * distances2[-1], 4.2],
     arrowprops=dict(arrowstyle="->", linewidth=3),
 )
 ab5 = AnnotationBbox(
